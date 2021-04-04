490addedb27449824f26beb4a170bd3b85cbb443
Pavel Petkov <pavelpetkovvv@gmail.com>
2021-04-05 01:15:27 +0300

    initial commit

---
+++ best_homes/js/main.js
@@ -0,0 +1,33 @@
+
+//slideshow JS
+var slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    console.log(slides);
+    console.log(slideIndex);
+    console.log(slides[slideIndex-1]);
+    slides[slideIndex-1].style.display = "block";
+  }
+
+showSlides(slideIndex);
+
+//change slide every 10 seconds
+function myLoop() {         
+    setTimeout(function() {  
+        plusSlides(1);
+      myLoop();
+    }, 10000)
+  }
+  myLoop();
+  
