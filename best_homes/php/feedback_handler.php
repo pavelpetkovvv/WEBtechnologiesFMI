260d3b97d82d7117962f46fbf54717dfeb3912d1
Pavel Petkov <pavelpetkovvv@gmail.com>
2021-04-05 14:50:53 +0300

    add contact form

---
+++ best_homes/php/feedback_handler.php
@@ -0,0 +1,18 @@
+<?php
+// Time to get the variables from the user’s request. Once we execute these four commands, we’ll have the user’s data in our variables.
+$name = $_POST['client_name'];
+$email = $_POST['client_email'];
+$message = $_POST['client_text'];
+
+// Creating the email message that we’ll send.
+$mes = "Name: $name \nE-mail: $email \nMessage: $message";
+
+// Trying to send the message using the PHP mailer module.
+$send = mail ("pavelpetkovvv@gmail.com","website feedback",$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+// If send successful:
+if ($send == 'true')
+// ‘Echo’ returns some text back to the webpage.
+{echo "Message sent";}
+// If send fails:
+else {echo "Something went wrong";}
+?>
