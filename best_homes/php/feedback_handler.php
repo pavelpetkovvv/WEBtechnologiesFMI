194182092ad70a6ab76a0068b030853f654b6c52
Pavel Petkov <pavelpetkovvv@gmail.com>
2021-04-09 02:10:26 +0300

    php removed for security reasons

--- best_homes/php/feedback_handler.php
+++ best_homes/php/feedback_handler.php
@@ -1,91 +1,36 @@
 <?php
-// Time to get the variables from the user’s request. Once we execute these four commands, we’ll have the user’s data in our variables.
+require_once "Mail.php";
+
 $name = $_POST['client_name'];
 $email = $_POST['client_email'];
 $message = $_POST['client_text'];
 
-// Creating the email message that we’ll send.
-$mes = "Name: $name \nE-mail: $email \nMessage: $message";
+$from = 'best.homes@abv.bg'; //change this to your email address
+$to = 'best.homes@abv.bg'; // change to address
+$subject = 'Contact form'; // subject of mail
+$body = "Name: $name \nE-mail: $email \nMessage: $message"; //content of mail
 
-// Trying to send the message using the PHP mailer module.
-$send = mail ("best.homes@abv.bg", "website feedback",$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:best.homes@abv.bg");
-// If send successful:
-if ($send == 'true')
-// ‘Echo’ returns some text back to the webpage.
-{echo '
-  <script>
-  window.history.back();
-  </script>';}
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
 
-  // If send fails:
-else {echo '                <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script  src="../js/main.js" defer></script>
+$smtp = Mail::factory('smtp', array(
+        'host' => 'smtp.abv.bg',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'best.homes@abv.bg', //your gmail account
+        'password' => 'besthomes01' // your password
+    ));
 
-  <title>Best homes</title>
+// Send the mail
+$mail = $smtp->send($to, $headers, $body);
 
-
-  <style>
-header{
-position: relative;
-text-align: center;
+//check mail sent or not
+if (PEAR::isError($mail)) {
+    echo '<p>Message was notsent! Try again latter</p>';
+} else {
+    echo '<p> meassage sent.</p>';
 }
-
-      .header_image_logo_container{
-      position: relative;
-      display: block;
-      float: left;
-      }
-      @media screen and (max-width: 710px) {
-      .site_heading_container{
-      display: none;
-      }
-      header{
-      display: block;
-      }
-      .header_image_logo_container {
-      position: relative;
-      float: none;
-      }
-      }
-      .site_heading{
-font-size: x-large;
-display:block;
-}
-
-.navigation{
-text-align: center;
-background-color: rgb(167, 40, 40);
-overflow: auto;
-}
-
-
-  </style>
-  <header>
-      <div class="header_top">
-          <div class="header_image_logo_container">
-              <img class="header_image_logo" src="../images/logo.jpg" alt="logo">
-          </div>
-          <div class="site_heading_container">
-              <p class="site_heading">BEST HOMES</p>
-          </div>
-      </div>
-  </header>
-
-  <div class="navigation">
-      <p class="site_heading">Възникна проблем при изпращането на съобщението, моля опитайте по-късно.</p>
-  </div>
-  <p>
-      Ще бъдете пренасочени към предишната страница.
-  </p>
-
-  <script>
-        function myLoop() {         
-setTimeout(function() {    
-  window.history.back();  
-}, 8000)
-}
-myLoop();
-
-  </script>';}
 ?>
