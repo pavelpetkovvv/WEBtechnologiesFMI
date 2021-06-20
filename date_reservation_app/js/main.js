efa69bc5caca19bf04d21efb658044a195fd6e46
Pavel Petkov <pavelpetkovvv@gmail.com>
2021-06-20 19:18:57 +0300

    add export data to csv feature

--- date_reservation_app/js/main.js
+++ date_reservation_app/js/main.js
@@ -590,6 +590,98 @@ function updateDataSendData(id, topic, presentationName, fn, presentorName, pass
 
     removeReservedSlot(id);
     renderReservedHourSlot(response.date, response.fn, response.presentationName, response.presentorName, response.time);
+    highlight();
   }
   }
 }
+
+
+function convertToCSV(objArray) {
+  console.log(objArray)
+  var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+  var str = '';
+  console.log(array)
+  for (var i = 0; i < array.length; i++) {
+      var line = '';
+      for (var index in array[i]) {
+          if (line != '') line += ','
+
+          line += array[i][index];
+      }
+
+      str += line + '\r\n';
+  }
+
+  return str;
+}
+
+function toCSV(){
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("POST", "./php/getReservedHours.php");
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send();
+
+  xhr.onload = function(){
+    let response = JSON.parse(this.responseText);
+
+    let items = [];
+      response.forEach((item) => {
+      items.push({
+          id: item.id, // remove commas to avoid errors,
+          presentationName: item.presentationName,
+          fn: item.fn,
+          presentorName: item.presentorName,
+          topic: item.topic,
+          date: item.date,
+          time: item.time
+      });
+  });
+
+    exportCSVFile(headers, items, fileTitle);
+  }
+}
+
+function exportCSVFile(headers, items, fileTitle) {
+  if (headers) {
+      items.unshift(headers);
+  }
+
+  // Convert Object to JSON
+  var jsonObject = JSON.stringify(items);
+
+  var csv = this.convertToCSV(jsonObject);
+
+  var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+
+  var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+  if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, exportedFilenmae);
+  } else {
+      var link = document.createElement("a");
+      if (link.download !== undefined) { // feature detection
+          // Browsers that support HTML5 download attribute
+          var url = URL.createObjectURL(blob);
+          link.setAttribute("href", url);
+          link.setAttribute("download", exportedFilenmae);
+          link.style.visibility = 'hidden';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+      }
+  }
+}
+
+var headers = {
+  id: 'ID', // remove commas to avoid errors
+  presentationName: "PresentationName",
+  fn: "FN",
+  presentorName: "Presentator name",
+  topic: "Topic",
+  date: "Date",
+  time: "Time",
+};
+
+var itemsFormatted = [];
+
+var fileTitle = 'file'; // or 'my-unique-title'
